d59622315e44dc5c1e89ef44200a879d3b5890b2
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2022-12-24 00:27:22 -0500

    font added

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { ThemeProvider } from '@emotion/react'
-import { CssBaseline } from '@mui/material'
 import theme from './theme/Theme'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <ThemeProvider theme={theme}>
-    <CssBaseline />
     <App />
   </ThemeProvider>,
 )
