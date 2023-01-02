c8c0e43a108d46925f12d87326f71a4ff4cbb617
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-01-01 23:00:50 -0500

    update - tilde icon

--- src/components/BestSelling.tsx
+++ src/components/BestSelling.tsx
@@ -2,6 +2,7 @@ import { Typography, Box, Link } from '@mui/material'
 import { Container } from './Container'
 import theme from '../theme/Theme'
 import { ArrowRightAlt } from '@mui/icons-material'
+import { TildeIcon } from '../icons/TildeIcon'
 
 export const BestSelling: React.FC = () => {
   const bestBook = {
@@ -61,19 +62,7 @@ export const BestSelling: React.FC = () => {
             <Typography fontSize={['40px', '35px', '48px']}>
               Best Selling Book
             </Typography>
-            <svg
-              width="48"
-              height="7"
-              viewBox="0 0 48 7"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1 1C1 1 4.36355 6 7.57143 6C10.7793 6 10.935 1 14.1429 1C17.3507 1 17.5064 6 20.7143 6C23.9222 6 24.0778 1 27.2857 1C30.4936 1 30.6493 6 33.8571 6C37.065 6 37.2207 1 40.4286 1C43.6365 1 47 6 47 6"
-                stroke="#74642F"
-                strokeWidth="2"
-              />
-            </svg>
+            <TildeIcon />
 
             <Typography
               color="text.secondary"
